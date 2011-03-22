d443a650056ce65ad40e6c999657a2d2c9ed58fe
siof <siof@localhost>
2011-03-22 12:50:05 +0100

    Init. Contains sample layout, menu and 1 page.

---
+++ menu.h
@@ -0,0 +1,25 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+#include <WMenu>
+#include <WStackedWidget>
+
+#include "defines.h"
+
+using namespace Wt;
+
+class HGMenu : public WContainerWidget
+{
+public:
+    HGMenu(WStackedWidget * menuContents, SessionInfo * sess, WContainerWidget *parent=0);
+    ~HGMenu();
+private:
+    WContainerWidget * container;       // contains menu + additional items added on menu side
+    WStackedWidget * menuContents;      // container where menu items will be shown after click
+    WMenu * menu;
+    WLineEdit * login;
+    WLineEdit * pass;
+    SessionInfo * session;
+};
+
+#endif // MENU_H_INCLUDED
