ac7ce0cdfa786aa0357db4fa7ba2df65c44713f1
siof <siof@siof.pl>
2011-05-26 14:49:01 +0200

    add licence, rename defines.h to defines.h.dist

--- menu.cpp
+++ menu.cpp
@@ -1,3 +1,21 @@
+/*
+*    HG Players Panel - web panel for HellGround server Players
+*    Copyright (C) 2011 HellGround Team : Siof, lukaasm,
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "menu.h"
 #include "pages/default.h"
 #include "database.h"
@@ -46,7 +64,7 @@ HGMenu::HGMenu(WStackedWidget * menuContents, SessionInfo * sess, WContainerWidg
     addWidget(breakTab[2]);
 
     menu = new WMenu(menuContents, Wt::Vertical, this);
-    menu->setRenderAsList(true);
+    menu->setRenderAsList(false);
     ShowMenuOptions();
 
     addWidget(menu);
